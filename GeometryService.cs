f4f6c17e977910ff1a84bcc67ac0aa5f5f48df7c
Romanova Svitlana <svetaromanova387@gmail.com>
2025-11-20 19:34:27 +0200

    first commit

---
+++ GeometryService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GeometryService
+    {
+        public double TriangleArea(double a, double h)
+        {
+            if (a <= 0 || h <= 0)
+                throw new ArgumentException("Сторона і висота повинні бути додатними.");
+
+            return (a * h) / 2;
+        }
+
+
+        public double RectangleDiagonal(double a, double b)
+        {
+            if (a <= 0 || b <= 0)
+                throw new ArgumentException("Сторони повинні бути додатними.");
+
+            return Math.Sqrt(a * a + b * b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Graph;
+using Microsoft.AspNetCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(Int32.Parse(port));
+});
